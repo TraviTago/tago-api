afdf4e9b00e2fbacc3eedf40491e39d1caaf0c9e
yerin03 <kyr2532@naver.com>
2023-07-27 09:44:26 +0900

    feat: Member 도메인

--- src/main/java/com/api/tago/common/config/ClientConfig.java
+++ src/main/java/com/api/tago/common/config/ClientConfig.java
@@ -1,4 +1,4 @@
-package com.api.tago.auth.common.config;
+package com.api.tago.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ src/main/java/com/api/tago/member/OAuthProvider.java
@@ -0,0 +1,6 @@
+package com.api.tago.member;
+
+public enum OAuthProvider {
+
+    KAKAO
+}

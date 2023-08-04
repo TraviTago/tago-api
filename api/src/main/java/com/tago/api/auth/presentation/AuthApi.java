0d8601b6adff758fdf5f8e604f88c3d21636f84b
김예린 <yell@gim-yelin-ui-MacBookAir.local>
2023-08-04 23:28:28 +0900

    feat: sdk로그인 방식 추가

--- api/src/main/java/com/tago/api/auth/presentation/AuthApi.java
+++ api/src/main/java/com/tago/api/auth/presentation/AuthApi.java
@@ -2,6 +2,8 @@ package com.tago.api.auth.presentation;
 
 
 import com.tago.api.auth.application.OAuthLoginService;
+import com.tago.api.auth.application.OAuthSdkService;
+import com.tago.api.auth.dto.LoginRequest;
 import com.tago.api.auth.dto.LoginResponse;
 import com.tago.api.auth.infra.kakao.KakaoLoginParams;
 import com.tago.api.common.model.dto.ResponseDto;
@@ -17,10 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/")
 public class AuthApi {
     private final OAuthLoginService oAuthLoginService;
+    private final OAuthSdkService oAuthSdkService;
 
     @PostMapping("/auth/kakao")
     public ResponseEntity<LoginResponse> loginKakao(@RequestBody KakaoLoginParams params) {
         return ResponseDto.ok(oAuthLoginService.login(params));
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<LoginResponse> loginKakao(@RequestBody LoginRequest loginRequest){
+        return ResponseDto.ok(oAuthSdkService.login(loginRequest));
     }
 
 }

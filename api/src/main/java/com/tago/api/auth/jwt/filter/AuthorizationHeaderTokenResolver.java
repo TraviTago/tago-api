985a66ac770e0464f90f2c8be2f9a8fe35497b32
twoosky <lhjae3@naver.com>
2023-08-05 18:06:17 +0900

    feat: 헤더로부터 토큰값 파싱 및 검증 객체 구현

---
+++ api/src/main/java/com/tago/api/auth/jwt/filter/AuthorizationHeaderTokenResolver.java
@@ -0,0 +1,38 @@
+package com.tago.api.auth.jwt.filter;
+
+import com.tago.api.auth.jwt.exception.AuthenticateException;
+import com.tago.api.auth.jwt.exception.ErrorMessages;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AuthorizationHeaderTokenResolver {
+    private static final String AUTHORIZATION_SCHEMA = "Bearer";
+
+    public String resolve(String value) {
+        String[] values = value.split(" ");
+        validate(values);
+        return values[1];
+    }
+
+    private void validate(String[] values) {
+        if (values.length != 2) {
+            throw new AuthenticateException(ErrorMessages.INVALID_TOKEN_HEADER);
+        }
+        validateSchema(values[0]);
+        validateToken(values[1]);
+    }
+    public void validateSchema(String schema) {
+        Optional.ofNullable(schema)
+                .orElseThrow(() -> new AuthenticateException(ErrorMessages.EMPTY_AUTHORIZATION_SCHEMA));
+        if (!schema.equals(AUTHORIZATION_SCHEMA)) {
+            throw new AuthenticateException(ErrorMessages.INVALID_AUTHORIZATION_SCHEMA);
+        }
+    }
+
+    public void validateToken(String token) {
+        Optional.ofNullable(token)
+                .orElseThrow(() -> new AuthenticateException(ErrorMessages.EMPTY_AUTHORIZATION_TOKEN));
+    }
+}

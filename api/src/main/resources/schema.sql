3d70db1b1712bf6827e951694d11c7dde69f4b55
twoosky <lhjae3@naver.com>
2023-08-05 18:28:25 +0900

    chore: db member ddl 추가

--- api/src/main/resources/schema.sql
+++ api/src/main/resources/schema.sql
@@ -1,7 +1,8 @@
 create table if not exists member (
     id bigint auto_increment,
-    email varchar(255) not null,
-    nick_name varchar(255),
+    email varchar(255),
+    name varchar(255) not null,
+    authority varchar(255) not null,
     oauth_provider varchar(255),
     primary key (id)
 );

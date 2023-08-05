8062a0fa30b7d3bcec2a1b316dbf012f445b1c94
twoosky <lhjae3@naver.com>
2023-08-05 18:24:59 +0900

    feat: authenticationEntryPoint 및 authException 구현

---
+++ api/src/main/java/com/tago/api/auth/jwt/exception/ErrorMessages.java
@@ -0,0 +1,15 @@
+package com.tago.api.auth.jwt.exception;
+
+public class ErrorMessages {
+    // JwtTokenExtractor
+    public static final String EXPIRED_TOKEN = "만료된 JWT 토큰입니다.";
+    public static final String UN_SUPPORTED_TOKEN = "지원되지 않는 형식의 JWT 토큰입니다.";
+    public static final String INVALID_TOKEN = "잘못된 형식의 JWT 토큰입니다.";
+    public static final String INVALID_TOKEN_HEADER = "잘못된 형식의 JWT 헤더값입니다.";
+
+    // AuthorizationHeaderTokenResolver
+    public static final String EMPTY_AUTHORIZATION_SCHEMA = "스키마가 존재하지 않습니다.";
+    public static final String INVALID_AUTHORIZATION_SCHEMA = "잘못된 형식의 스키마입니다.";
+    public static final String EMPTY_AUTHORIZATION_TOKEN = "토큰이 존재하지 않습니다.";
+
+}

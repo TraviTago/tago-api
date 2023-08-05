35548e8672ebce07b9b6cb189a2232601d053d86
twoosky <lhjae3@naver.com>
2023-08-05 18:00:56 +0900

    refactor: domainApplication main 메서드 삭제

--- domain/src/main/java/com/tago/domain/DomainApplication.java
+++ domain/src/main/java/com/tago/domain/DomainApplication.java
@@ -1,6 +1,7 @@
 package com.tago.domain;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 public class DomainApplication {

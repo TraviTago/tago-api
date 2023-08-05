f1bb587b1d078d4a42d185a8b0124b306d16bc6b
twoosky <lhjae3@naver.com>
2023-08-05 18:27:20 +0900

    feat: apiApplication에 ConfigurationPropertiesScan 추가

--- api/src/main/java/com/tago/api/ApiApplication.java
+++ api/src/main/java/com/tago/api/ApiApplication.java
@@ -2,7 +2,9 @@ package com.tago.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication(scanBasePackages = {"com.tago.api", "com.tago.domain"})
 public class ApiApplication {
 

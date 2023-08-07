bf6d6ba6b9133264c6eba604a6fd791ff523b2cf
twoosky <lhjae3@naver.com>
2023-08-08 02:31:59 +0900

    feat: 멤버 정보 profile 임베디드 타입 객체 선언

---
+++ domain/src/main/java/com/tago/domain/member/domain/vo/Profile.java
@@ -0,0 +1,39 @@
+package com.tago.domain.member.domain.vo;
+
+import com.tago.domain.common.converter.FavoriteEnumArrayConverter;
+import com.tago.domain.common.converter.TripTypeEnumArrayConverter;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lombok.Builder.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Embeddable
+public class Profile {
+
+    @Default
+    @Column(name = "age_range")
+    private Integer ageRange;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    private Mbti mbti;
+
+    @Default
+    @Convert(converter = FavoriteEnumArrayConverter.class)
+    private List<Favorite> favorites = new ArrayList<>();
+
+    @Default
+    @Column(name = "trip_types")
+    @Convert(converter = TripTypeEnumArrayConverter.class)
+    private List<TripType> tripTypes = new ArrayList<>();
+}

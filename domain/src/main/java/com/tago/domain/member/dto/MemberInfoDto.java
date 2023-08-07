05c0b15c047b8ad9378ded63afe1468589dd6049
twoosky <lhjae3@naver.com>
2023-08-08 02:32:37 +0900

    feat: 멤버 정보 dto 작성

---
+++ domain/src/main/java/com/tago/domain/member/dto/MemberInfoDto.java
@@ -0,0 +1,24 @@
+package com.tago.domain.member.dto;
+
+import com.tago.domain.member.domain.vo.Favorite;
+import com.tago.domain.member.domain.vo.Gender;
+import com.tago.domain.member.domain.vo.Mbti;
+import com.tago.domain.member.domain.vo.TripType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberInfoDto {
+    private int ageRange;
+    private Gender gender;
+    private Mbti mbti;
+    private List<Favorite> favorites;
+    private List<TripType> tripTypes;
+}

283c5e113c8f50156bdc4510dedfabfd5b38dd6e
twoosky <lhjae3@naver.com>
2023-08-08 02:31:15 +0900

    mbti enum 생성

---
+++ domain/src/main/java/com/tago/domain/member/domain/vo/Mbti.java
@@ -0,0 +1,24 @@
+package com.tago.domain.member.domain.vo;
+
+import lombok.Getter;
+
+@Getter
+public enum Mbti {
+    ENFJ,
+    ENTJ,
+    ENFP,
+    ENTP,
+    ESFP,
+    ESFJ,
+    ESTP,
+    ESTJ,
+    INFP,
+    INFJ,
+    INTP,
+    ISTP,
+    ISFP,
+    ISFJ,
+    ISTJ,
+    INTJ
+
+}

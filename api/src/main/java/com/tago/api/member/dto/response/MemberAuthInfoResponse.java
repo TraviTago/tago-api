804aa940b77329305c0f144da3c0b9613c660fc9
twoosky <lhjae3@naver.com>
2023-08-08 18:25:07 +0900

    feat: member 인증 정보 반환 dto 작성

---
+++ api/src/main/java/com/tago/api/member/dto/response/MemberAuthInfoResponse.java
@@ -0,0 +1,15 @@
+package com.tago.api.member.dto.response;
+
+import com.tago.domain.member.domain.vo.OAuthProvider;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberAuthInfoResponse {
+    private String email;
+    private OAuthProvider oAuthProvider;
+    private boolean isSignedUp;
+}

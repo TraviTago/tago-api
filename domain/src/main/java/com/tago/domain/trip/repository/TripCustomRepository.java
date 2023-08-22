7ceeba02d37e0a7b140c4ba5258c2303cb2c48b9
twoosky <lhjae3@naver.com>
2023-08-23 01:08:01 +0900

    refactor: 필요없는 import 제거

--- domain/src/main/java/com/tago/domain/trip/repository/TripCustomRepository.java
+++ domain/src/main/java/com/tago/domain/trip/repository/TripCustomRepository.java
@@ -1,11 +1,12 @@
 package com.tago.domain.trip.repository;
 
 import com.tago.domain.trip.domain.Trip;
-import com.tago.domain.trip.dto.TripPreviewDto;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TripCustomRepository {
+
     List<Trip> findAllFetchTripPlaceAndPlace(Long cursorId, LocalDateTime cursorDate, int limit);
 }
+

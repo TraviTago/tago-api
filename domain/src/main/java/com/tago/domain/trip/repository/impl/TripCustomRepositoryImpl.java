4e266053d67cb64f5f32f256d35820c87be2e69a
twoosky <lhjae3@naver.com>
2023-08-22 22:49:51 +0900

    feat: trip 전체 조회 fetch join query 작성

---
+++ domain/src/main/java/com/tago/domain/trip/repository/impl/TripCustomRepositoryImpl.java
@@ -0,0 +1,51 @@
+package com.tago.domain.trip.repository.impl;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.tago.domain.trip.domain.Trip;
+import com.tago.domain.trip.repository.TripCustomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static com.tago.domain.place.domain.QPlace.place;
+import static com.tago.domain.trip.domain.QTrip.trip;
+import static com.tago.domain.trip.domain.QTripPlace.tripPlace;
+
+@Repository
+@RequiredArgsConstructor
+public class TripCustomRepositoryImpl implements TripCustomRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<Trip> findAllFetchTripPlaceAndPlace(Long cursorId, LocalDateTime cursorDate, int limit) {
+        return queryFactory.selectFrom(trip)
+                .leftJoin(trip.tripPlaces, tripPlace).fetchJoin()
+                .leftJoin(tripPlace.place, place).fetchJoin()
+                .where(
+                        isNotDone(),
+                        cursorGt(cursorId, cursorDate)
+                )
+                .orderBy(trip.dateTime.desc(), tripPlace.order.asc())
+                .limit(limit)
+                .fetch();
+    }
+
+    private BooleanExpression cursorGt(Long cursorId, LocalDateTime cursorDate) {
+        return cursorIdAndDateGt(cursorId, cursorDate).or(cursorDateGt(cursorDate));
+    }
+
+    private BooleanExpression cursorIdAndDateGt(Long cursorId, LocalDateTime cursorDate) {
+        return trip.id.gt(cursorId).and(trip.dateTime.eq(cursorDate));
+    }
+    private BooleanExpression cursorDateGt(LocalDateTime cursorDate) {
+        return trip.dateTime.gt(cursorDate); // trip.datetime > cursorDate
+    }
+
+    private BooleanExpression isNotDone() {
+        return trip.dateTime.gt(LocalDateTime.now()); // trip.datetime > now
+    }
+}

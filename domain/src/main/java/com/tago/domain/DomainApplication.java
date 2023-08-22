ef432b9c844e153fa4645b4f2e7d59a0f0dc3d81
twoosky <lhjae3@naver.com>
2023-08-22 22:54:17 +0900

    refactor: 사용하지 않는 import 정리

--- domain/src/main/java/com/tago/domain/DomainApplication.java
+++ domain/src/main/java/com/tago/domain/DomainApplication.java
@@ -1,7 +1,6 @@
 package com.tago.domain;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 public class DomainApplication {

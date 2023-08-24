264ba8423e0cffa049c79538c8cb9cf6aa4afdea
twoosky <lhjae3@naver.com>
2023-08-24 19:15:02 +0900

    feat: trip,place 조회 비즈니스 로직 작성

---
+++ api/src/main/java/com/tago/api/trip/application/TripPlaceService.java
@@ -0,0 +1,32 @@
+package com.tago.api.trip.application;
+
+import com.tago.api.trip.dto.response.TripPlaceGetResponse;
+import com.tago.domain.trip.domain.Trip;
+import com.tago.domain.trip.domain.TripPlace;
+import com.tago.domain.trip.dto.TripPlaceDto;
+import com.tago.domain.trip.service.TripPlaceQueryService;
+import com.tago.domain.trip.service.TripQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TripPlaceService {
+
+    private final TripQueryService tripQueryService;
+    private final TripPlaceQueryService tripPlaceQueryService;
+
+    public TripPlaceGetResponse getTripAndPlaces(Long tripId) {
+        Trip trip = tripQueryService.findById(tripId);
+        List<TripPlaceDto> tripPlaces = tripPlaceQueryService.findAll(trip);
+
+        return new TripPlaceGetResponse(
+                trip.getName(),
+                trip.getCurrentCnt(),
+                trip.getMaxCnt(),
+                tripPlaces
+        );
+    }
+}

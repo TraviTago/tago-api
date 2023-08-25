f0f1968251b76155e4fcdcddab28683a20d5843b
twoosky <lhjae3@naver.com>
2023-08-26 03:06:14 +0900

    fix: string타입  enum 변환 메서드 작성

--- domain/src/main/java/com/tago/domain/member/domain/vo/Gender.java
+++ domain/src/main/java/com/tago/domain/member/domain/vo/Gender.java
@@ -1,9 +1,27 @@
 package com.tago.domain.member.domain.vo;
 
+import com.tago.domain.common.error.ErrorCode;
+import com.tago.domain.member.exception.EnumNotFoundException;
 import lombok.Getter;
+
+import java.util.Arrays;
 
 @Getter
 public enum Gender {
-    MALE,
-    FEMALE
+    MALE("남성"),
+    FEMALE("여성"),
+    ;
+
+    private String description;
+
+    private Gender(String description) {
+        this.description = description;
+    }
+
+    public static Gender from(String gender) {
+        return Arrays.stream(Gender.values())
+                .filter(value -> value.description.equals(gender))
+                .findFirst()
+                .orElseThrow(() -> new EnumNotFoundException(ErrorCode.GENDER_NOT_FOUND));
+    }
 }

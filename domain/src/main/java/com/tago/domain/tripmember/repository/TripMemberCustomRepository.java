7622deb81c86564ecb2bb2e1e58eb0cb7dbe05a8
twoosky <lhjae3@naver.com>
2023-08-26 19:18:50 +0900

    feat: tirpMember custom Repository 생성

--- domain/src/main/java/com/tago/domain/tripmember/repository/TripMemberCustomRepository.java
+++ domain/src/main/java/com/tago/domain/tripmember/repository/TripMemberCustomRepository.java
@@ -3,9 +3,11 @@ package com.tago.domain.tripmember.repository;
 import com.tago.domain.member.domain.Member;
 import com.tago.domain.trip.domain.Trip;
 import com.tago.domain.tripmember.dto.MyTripDto;
+import com.tago.domain.tripmember.dto.TripMemberDto;
 
 import java.util.List;
 
 public interface TripMemberCustomRepository {
     List<Trip> findTripsByMember(Member member);
+    List<TripMemberDto> findMembersByTrip(Trip trip);
 }

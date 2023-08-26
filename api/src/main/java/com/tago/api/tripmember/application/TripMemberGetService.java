411c9462fbef495c04e647bab85e5fd2e138ba8d
twoosky <lhjae3@naver.com>
2023-08-26 19:21:27 +0900

    feat: trip 메이트 조회 application service 작성

--- api/src/main/java/com/tago/api/tripmember/application/TripMemberGetService.java
+++ api/src/main/java/com/tago/api/tripmember/application/TripMemberGetService.java
@@ -1,9 +1,13 @@
 package com.tago.api.tripmember.application;
 
+import com.tago.api.tripmember.dto.response.TripMemberGetMemberResponse;
 import com.tago.api.tripmember.dto.response.TripMemberGetTripResponse;
 import com.tago.domain.member.domain.Member;
 import com.tago.domain.member.service.MemberQueryService;
+import com.tago.domain.trip.domain.Trip;
+import com.tago.domain.trip.service.TripQueryService;
 import com.tago.domain.tripmember.dto.MyTripDto;
+import com.tago.domain.tripmember.dto.TripMemberDto;
 import com.tago.domain.tripmember.service.TripMemberQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,9 +17,10 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class TripMemberGetTripService {
+public class TripMemberGetService {
 
     private final MemberQueryService memberQueryService;
+    private final TripQueryService tripQueryService;
     private final TripMemberQueryService tripMemberQueryService;
 
     @Transactional(readOnly = true)
@@ -23,5 +28,12 @@ public class TripMemberGetTripService {
         Member member = memberQueryService.findById(memberId);
         List<MyTripDto> trips = tripMemberQueryService.findTripsByMember(member);
         return new TripMemberGetTripResponse(trips);
+    }
+
+    @Transactional(readOnly = true)
+    public TripMemberGetMemberResponse getMembersByTrip(Long tripId) {
+        Trip trip = tripQueryService.findById(tripId);
+        List<TripMemberDto> members = tripMemberQueryService.findMembersByTrip(trip);
+        return new TripMemberGetMemberResponse(members);
     }
 }

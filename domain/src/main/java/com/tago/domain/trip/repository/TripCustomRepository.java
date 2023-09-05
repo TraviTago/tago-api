0ed27726d7ce4474300eaded14973cb539068efb
twoosky <lhjae3@naver.com>
2023-09-05 23:29:21 +0900

    refactor: trip 전체 조회 pagenation 쿼리 최적화

--- domain/src/main/java/com/tago/domain/trip/repository/TripCustomRepository.java
+++ domain/src/main/java/com/tago/domain/trip/repository/TripCustomRepository.java
@@ -1,6 +1,8 @@
 package com.tago.domain.trip.repository;
 
 import com.tago.domain.trip.domain.Trip;
+import com.tago.domain.trip.domain.TripPlace;
+import com.tago.domain.trip.dto.TripPreviewDto;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -8,5 +10,6 @@ import java.util.List;
 public interface TripCustomRepository {
 
     List<Trip> findAllFetchTripPlaceAndPlace(Long cursorId, LocalDateTime cursorDate, int limit);
+    List<Trip> findByPlaceTitleKeywordContain(String keyword, Long cursorId, LocalDateTime cursorDate, int limit);
 }
 

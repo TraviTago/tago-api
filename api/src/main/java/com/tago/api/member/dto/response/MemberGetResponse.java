0a3aa36216eb979da90d2dfb018cfd245b44aea4
twoosky <lhjae3@naver.com>
2023-09-12 00:46:06 +0900

    refactor: slack 관련 코드 infra 패키지로 분리

---
+++ api/src/main/java/com/tago/api/member/dto/response/MemberGetResponse.java
@@ -0,0 +1,26 @@
+package com.tago.api.member.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberInfoResponse {
+    private String number;
+    private String name;
+    private String imgUrl;
+    private Integer ageRange;
+    private String gender;
+    private String mbti;
+    private List<String> favorites;
+    private List<String> tripTypes;
+}
+
+
+
+
+

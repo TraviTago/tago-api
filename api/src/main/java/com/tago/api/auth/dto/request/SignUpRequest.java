0e29ef0a57b0cc2ae625f7c36cfae16f6defc89d
twoosky <lhjae3@naver.com>
2023-09-12 00:53:14 +0900

    fix: 회원가입 request dto에서 email제거, phoneNumber 추가

--- api/src/main/java/com/tago/api/auth/dto/request/SignUpRequest.java
+++ api/src/main/java/com/tago/api/auth/dto/request/SignUpRequest.java
@@ -1,16 +1,16 @@
 package com.tago.api.auth.dto.request;
 
+import com.tago.api.common.annotation.PhoneNumberPattern;
 import com.tago.domain.member.domain.vo.Favorite;
 import com.tago.domain.member.domain.vo.Gender;
 import com.tago.domain.member.domain.vo.Mbti;
 import com.tago.domain.member.domain.vo.TripType;
-import com.tago.domain.member.dto.MemberInfoDto;
+import com.tago.domain.member.dto.MemberCreateDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Collection;
 import java.util.List;
 
 @Getter
@@ -18,14 +18,22 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignUpRequest {
+
+    @PhoneNumberPattern
+    private String number;
+    private String name;
+    private String imgUrl;
     private int ageRange;
     private String gender;
     private String mbti;
     private List<String> favorites;
     private List<String> tripTypes;
 
-    public MemberInfoDto toMemberInfoDto() {
-        return MemberInfoDto.builder()
+    public MemberCreateDto toMemberInfoDto() {
+        return MemberCreateDto.builder()
+                .number(number)
+                .name(name)
+                .imgUrl(imgUrl)
                 .ageRange(ageRange)
                 .gender(Gender.from(gender))
                 .mbti(Mbti.valueOf(mbti))

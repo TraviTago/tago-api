12b3c64ed2d24e79ed74a72a3a7feb7c5f6256fa
twoosky <lhjae3@naver.com>
2023-09-12 00:58:57 +0900

    fix: login, signup 비즈니스 로직 수정

--- api/src/main/java/com/tago/api/auth/application/AuthService.java
+++ api/src/main/java/com/tago/api/auth/application/AuthService.java
@@ -15,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class LoginService {
+public class AuthService {
 
     private final MemberQueryService memberQueryService;
     private final MemberCreateService memberCreateService;
--- api/src/main/java/com/tago/api/auth/application/OAuthLoginService.java
+++ api/src/main/java/com/tago/api/auth/application/OAuthLoginService.java
@@ -16,31 +16,31 @@ public class OAuthLoginService {
     private final JwtTokenGenerator jwtTokenGenerator;
     private final OAuthInfoService oAuthInfoService;
 
-    public LoginResponse login(OAuthLoginParams params) {
-        OAuthInfoResponse oAuthInfoResponse = oAuthInfoService.request(params);
-        Long memberId = findOrCreateMember(oAuthInfoResponse);
-        return generateTokenToDto(memberId);
-    }
-
-    private Long findOrCreateMember(OAuthInfoResponse oAuthInfoResponse) {
-        return memberRepository.findByEmail(oAuthInfoResponse.getEmail())
-                .map(Member::getId)
-                .orElseGet(() -> createMember(oAuthInfoResponse));
-    }
-
-    private Long createMember(OAuthInfoResponse oAuthInfoResponse) {
-        Member member = Member.builder()
-                .email(oAuthInfoResponse.getEmail())
-                .name(oAuthInfoResponse.getNickname())
-                .oauthProvider(oAuthInfoResponse.getOAuthProvider())
-                .build();
-
-        return memberRepository.save(member).getId();
-    }
-
-    public LoginResponse generateTokenToDto(Long memberId) {
-        String accessToken = jwtTokenGenerator.generateAccessToken(memberId);
-        String refreshToken = jwtTokenGenerator.generateRefreshToken(memberId);
-        return new LoginResponse(accessToken, refreshToken);
-    }
+//    public LoginResponse login(OAuthLoginParams params) {
+//        OAuthInfoResponse oAuthInfoResponse = oAuthInfoService.request(params);
+//        Long memberId = findOrCreateMember(oAuthInfoResponse);
+//        return generateTokenToDto(memberId);
+//    }
+//
+//    private Long findOrCreateMember(OAuthInfoResponse oAuthInfoResponse) {
+//        return memberRepository.findByEmail(oAuthInfoResponse.getEmail())
+//                .map(Member::getId)
+//                .orElseGet(() -> createMember(oAuthInfoResponse));
+//    }
+//
+//    private Long createMember(OAuthInfoResponse oAuthInfoResponse) {
+//        Member member = Member.builder()
+//                .email(oAuthInfoResponse.getEmail())
+//                .name(oAuthInfoResponse.getNickname())
+//                .oauthProvider(oAuthInfoResponse.getOAuthProvider())
+//                .build();
+//
+//        return memberRepository.save(member).getId();
+//    }
+//
+//    public LoginResponse generateTokenToDto(Long memberId) {
+//        String accessToken = jwtTokenGenerator.generateAccessToken(memberId);
+//        String refreshToken = jwtTokenGenerator.generateRefreshToken(memberId);
+//        return new LoginResponse(accessToken, refreshToken);
+//    }
 }

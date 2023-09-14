2f9afc57545cf88ddeb91590ac5f7fabc003462f
twoosky <lhjae3@naver.com>
2023-09-15 02:33:46 +0900

    feat: type에 대한 tag 조회 쿼리 메서드 작성

---
+++ domain/src/main/java/com/tago/domain/tag/repository/impl/TagCustomRepositoryImpl.java
@@ -0,0 +1,31 @@
+package com.tago.domain.tag.repository.impl;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.tago.domain.member.domain.vo.Favorite;
+import com.tago.domain.tag.domain.Tag;
+import com.tago.domain.tag.repository.TagCustomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.tago.domain.tag.domain.QTag.tag;
+
+@Repository
+@RequiredArgsConstructor
+public class TagCustomRepositoryImpl implements TagCustomRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<Tag> findAllByType(List<Favorite> types) {
+        return queryFactory.selectFrom(tag)
+                .where(typeIn(types))
+                .fetch();
+    }
+
+    private BooleanExpression typeIn(List<Favorite> types) {
+        return tag.type.in(types);
+    }
+}

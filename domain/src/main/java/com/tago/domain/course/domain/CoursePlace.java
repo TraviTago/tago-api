9f20ae52c8328538afe14caae75ee6c4dcd95776
twoosky <lhjae3@naver.com>
2023-09-14 20:04:17 +0900

    feat: course place 엔티티 작성

--- domain/src/main/java/com/tago/domain/course/domain/CoursePlace.java
+++ domain/src/main/java/com/tago/domain/course/domain/CoursePlace.java
@@ -3,6 +3,7 @@ package com.tago.domain.course.domain;
 import com.tago.domain.place.domain.Place;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.BatchSize;
 
 @Getter
 @Builder
@@ -24,7 +25,20 @@ public class CoursePlace {
     @JoinColumn(name = "course_id")
     private Course course;
 
+    @BatchSize(size = 10)
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "place_id")
     private Place place;
+
+    public Long getPlaceId() {
+        return this.place.getId();
+    }
+
+    public String getPlaceTitle() {
+        return this.place.getTitle();
+    }
+
+    public String getPlaceImgUrl() {
+        return this.place.getImgUrl();
+    }
 }

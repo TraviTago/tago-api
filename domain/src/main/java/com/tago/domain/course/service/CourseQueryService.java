77064739c86fa6a62338acf7cd81b2b7840e6992
twoosky <lhjae3@naver.com>
2023-09-14 20:03:05 +0900

    feat: course tag 검색 쿼리 service 작성

---
+++ domain/src/main/java/com/tago/domain/course/service/CourseQueryService.java
@@ -0,0 +1,20 @@
+package com.tago.domain.course.service;
+
+import com.tago.domain.course.domain.Course;
+import com.tago.domain.course.repository.CourseRepository;
+import com.tago.domain.member.domain.vo.Favorite;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CourseQueryService {
+
+    private final CourseRepository courseRepository;
+
+    public Course findByPlaceIdAndCourseTag(Long placeId, List<Favorite> tags) {
+        return courseRepository.findByPlaceIdAndCourseTag(placeId, tags);
+    }
+}

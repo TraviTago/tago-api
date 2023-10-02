6ef3e5916f4a31f1b59962b96a20c0117ce88076
twoosky <lhjae3@naver.com>
2023-10-02 19:21:18 +0900

    refactor: trip 참여 서비스 tripMemberRepository 의존성 제거

--- api/src/main/java/com/tago/api/tripmember/application/TripMemberSaveService.java
+++ api/src/main/java/com/tago/api/tripmember/application/TripMemberSaveService.java
@@ -1,11 +1,11 @@
 package com.tago.api.tripmember.application;
 
 
-import com.tago.api.tripmember.dto.response.TripMemberJoinResponse;
+import com.tago.api.common.exception.AlreadyExistsTripMemberException;
 import com.tago.domain.member.domain.Member;
 import com.tago.domain.member.handler.MemberQueryService;
 import com.tago.domain.trip.domain.Trip;
-import com.tago.domain.tripmember.domain.TripMember;
+import com.tago.domain.tripmember.handler.TripMemberQueryService;
 import com.tago.domain.tripmember.service.TripMemberCreateService;
 import com.tago.domain.trip.handler.TripQueryService;
 import jakarta.transaction.Transactional;
@@ -17,18 +17,22 @@ import org.springframework.stereotype.Service;
 public class TripMemberSaveService {
 
     private final TripMemberCreateService tripMemberCreateService;
+    private final TripMemberQueryService tripMemberQueryService;
     private final TripQueryService tripQueryService;
     private final MemberQueryService memberQueryService;
 
     @Transactional
-    public TripMemberJoinResponse joinTrip(Long tripId, Long memberId){
-        Trip trip = tripQueryService.findById(tripId);
+    public void joinTrip(Long tripId, Long memberId){
+        isAlreadyJoined(tripId, memberId);
+        Trip trip = tripQueryService.findByIdFetchTripMember(tripId);
         Member member = memberQueryService.findById(memberId);
+        trip.join(member);
+    }
 
-        trip.join();
-        TripMember tripMember = tripMemberCreateService.create(trip, member);
-
-        return new TripMemberJoinResponse(tripMember.getId());
+    private void isAlreadyJoined(Long tripId, Long memberId) {
+        if (tripMemberQueryService.existsByTripIdAndMemberId(tripId, memberId)) {
+            throw new AlreadyExistsTripMemberException();
+        }
     }
 }
 

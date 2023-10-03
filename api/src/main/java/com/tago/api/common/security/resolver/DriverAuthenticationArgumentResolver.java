33cdddc76347a969eddd0659b8fc149283138697
twoosky <lhjae3@naver.com>
2023-10-03 17:37:41 +0900

    fix: driverAuthentication argumentResolver 작성

--- api/src/main/java/com/tago/api/common/security/resolver/DriverAuthenticationArgumentResolver.java
+++ api/src/main/java/com/tago/api/common/security/resolver/DriverAuthenticationArgumentResolver.java
@@ -1,10 +1,13 @@
 package com.tago.api.common.security.resolver;
 
 import com.tago.api.common.exception.ResolveArgumentException;
-import com.tago.api.common.security.annotation.UserAuthentication;
+import com.tago.api.common.security.annotation.DriverAuthentication;
+import com.tago.api.common.security.jwt.exception.InvalidAuthorityException;
 import com.tago.domain.common.error.ErrorCode;
+import com.tago.domain.member.domain.vo.Role;
 import org.springframework.core.MethodParameter;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -12,12 +15,14 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import java.util.Collection;
+
 @Component
 public class DriverAuthenticationArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        boolean isLoginMemberAnnotation = parameter.hasParameterAnnotation(UserAuthentication.class);
+        boolean isLoginMemberAnnotation = parameter.hasParameterAnnotation(DriverAuthentication.class);
         boolean isLongType = Long.class.isAssignableFrom(parameter.getParameterType());
         return isLoginMemberAnnotation && isLongType;
     }
@@ -26,6 +31,7 @@ public class DriverAuthenticationArgumentResolver implements HandlerMethodArgume
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         validatePrincipalType(authentication);
+        validateRole(authentication);
         return authentication.getPrincipal();
     }
 
@@ -33,5 +39,13 @@ public class DriverAuthenticationArgumentResolver implements HandlerMethodArgume
         if (!(authentication.getPrincipal() instanceof Long)) {
             throw new ResolveArgumentException(ErrorCode.PRINCIPAL_TYPE_NOT_MATCH);
         }
+    }
+
+    private void validateRole(Authentication authentication) {
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        authorities.stream()
+                .filter(grantedAuthority -> grantedAuthority.getAuthority().equals(Role.DRIVER.name()))
+                .findFirst()
+                .orElseThrow(InvalidAuthorityException::new);
     }
 }

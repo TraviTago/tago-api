6ffe0e88a7e8f8c9998ac0431393bb2acd75bab3
twoosky <lhjae3@naver.com>
2023-10-03 20:03:27 +0900

    feat: trip,driver에 대한 dispatch 조회 쿼리메서드 작성

--- domain/src/main/java/com/tago/domain/driver/repository/DispatchRepository.java
+++ domain/src/main/java/com/tago/domain/driver/repository/DispatchRepository.java
@@ -1,7 +1,12 @@
 package com.tago.domain.driver.repository;
 
 import com.tago.domain.driver.domain.Dispatch;
+import com.tago.domain.driver.domain.Driver;
+import com.tago.domain.trip.domain.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DispatchRepository extends JpaRepository<Dispatch,Long> {
+    Optional<Dispatch> findByTripAndDriver(Trip trip, Driver driver);
 }

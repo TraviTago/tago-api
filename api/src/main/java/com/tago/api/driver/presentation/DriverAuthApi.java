1cae835ba548832eb9c08eeabf50f58520d18d8b
twoosky <lhjae3@naver.com>
2023-10-03 18:26:48 +0900

    feat: driver 로그인 api 작성

---
+++ api/src/main/java/com/tago/api/driver/presentation/DriverAuthApi.java
@@ -0,0 +1,26 @@
+package com.tago.api.driver.presentation;
+
+import com.tago.api.common.dto.ResponseDto;
+import com.tago.api.driver.application.DriverAuthService;
+import com.tago.api.driver.dto.request.LoginRequest;
+import com.tago.api.driver.dto.response.LoginResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/taxi/auth")
+public class DriverAuthApi {
+
+    private final DriverAuthService driverAuthService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        LoginResponse response = driverAuthService.login(request);
+        return ResponseDto.ok(response);
+    }
+}

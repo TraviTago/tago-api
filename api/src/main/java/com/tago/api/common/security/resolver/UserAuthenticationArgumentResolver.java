d374690aa0755ac383b269106925a417bb7376d7
twoosky <lhjae3@naver.com>
2023-10-03 17:37:55 +0900

    fix: userAuthentication argumentResolver 작성

--- api/src/main/java/com/tago/api/common/security/resolver/UserAuthenticationArgumentResolver.java
+++ api/src/main/java/com/tago/api/common/security/resolver/UserAuthenticationArgumentResolver.java
@@ -2,15 +2,20 @@ package com.tago.api.common.security.resolver;
 
 import com.tago.api.common.security.annotation.UserAuthentication;
 import com.tago.api.common.exception.ResolveArgumentException;
+import com.tago.api.common.security.jwt.exception.InvalidAuthorityException;
 import com.tago.domain.common.error.ErrorCode;
+import com.tago.domain.member.domain.vo.Role;
 import org.springframework.core.MethodParameter;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.Collection;
 
 @Component
 public class UserAuthenticationArgumentResolver implements HandlerMethodArgumentResolver {
@@ -26,6 +31,7 @@ public class UserAuthenticationArgumentResolver implements HandlerMethodArgument
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         validatePrincipalType(authentication);
+        validateRole(authentication);
         return authentication.getPrincipal();
     }
 
@@ -33,5 +39,13 @@ public class UserAuthenticationArgumentResolver implements HandlerMethodArgument
         if (!(authentication.getPrincipal() instanceof Long)) {
             throw new ResolveArgumentException(ErrorCode.PRINCIPAL_TYPE_NOT_MATCH);
         }
+    }
+
+    private void validateRole(Authentication authentication) {
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        authorities.stream()
+                .filter(grantedAuthority -> grantedAuthority.getAuthority().equals(Role.USER.name()))
+                .findFirst()
+                .orElseThrow(InvalidAuthorityException::new);
     }
 }

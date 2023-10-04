b0d0cf0e7659d11123e303d365d0aca87715fac6
twoosky <lhjae3@naver.com>
2023-10-04 16:31:15 +0900

    feat: driver 내 여행 조회 response dto 생성

---
+++ api/src/main/java/com/tago/api/driver/dto/response/DriverTripResponse.java
@@ -0,0 +1,14 @@
+package com.tago.api.driver.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DriverTripResponse {
+    List<TripGetResponse> trips;
+}

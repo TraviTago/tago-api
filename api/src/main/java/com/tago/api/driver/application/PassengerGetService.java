ab3cda85506a711d3850c099609b11c5dbcdd8ef
yerin03 <kyr2532@soongsil.ac.kr>
2023-10-08 15:28:04 +0900

    feat: 탑승객 정보 조회 api 구현

---
+++ api/src/main/java/com/tago/api/driver/application/PassengerGetService.java
@@ -0,0 +1,26 @@
+package com.tago.api.driver.application;
+
+import com.tago.api.driver.dto.response.PassengerGetResponse;
+import com.tago.domain.driver.dto.PassengerDto;
+import com.tago.domain.driver.service.PassengerQueryService;
+import com.tago.domain.trip.domain.Trip;
+import com.tago.domain.trip.handler.TripQueryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PassengerGetService {
+    private final PassengerQueryService passengerQueryService;
+    private final TripQueryService tripQueryService;
+
+    @Transactional(readOnly = true)
+    public PassengerGetResponse getPassengerByTip(Long tripId){
+        Trip trip = tripQueryService.findByIdFetchTripMember(tripId);
+        List<PassengerDto> members = passengerQueryService.findPassengerByTrip(trip);
+        return new PassengerGetResponse(members);
+    }
+}

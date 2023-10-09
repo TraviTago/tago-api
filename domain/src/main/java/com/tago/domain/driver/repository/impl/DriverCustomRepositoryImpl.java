03d85cedc34d105ef76c536aaca187eb902f0a83
twoosky <lhjae3@naver.com>
2023-10-09 13:02:54 +0900

    feat: driver 정보 반환 쿼리 메서드 파라미터 수정

--- domain/src/main/java/com/tago/domain/driver/repository/impl/DriverCustomRepositoryImpl.java
+++ domain/src/main/java/com/tago/domain/driver/repository/impl/DriverCustomRepositoryImpl.java
@@ -2,6 +2,8 @@ package com.tago.domain.driver.repository.impl;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.tago.domain.driver.domain.Driver;
+import com.tago.domain.driver.domain.QDriver;
 import com.tago.domain.driver.dto.DriverInfoDto;
 import com.tago.domain.driver.dto.QDriverInfoDto;
 import com.tago.domain.driver.repository.DriverCustomRepository;
@@ -20,26 +22,26 @@ public class DriverCustomRepositoryImpl implements DriverCustomRepository {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Optional<DriverInfoDto> findById(int cnt, Long driverId) {
+    public Optional<DriverInfoDto> findByDriverAndCar(int cnt, Driver driver) {
         return Optional.ofNullable(queryFactory.select(new QDriverInfoDto(
-                        driver.info.imgUrl,
-                        driver.info.name,
-                        driver.info.comment,
-                        driver.info.phoneNumber,
-                        driver.info.license,
+                        QDriver.driver.info.imgUrl,
+                        QDriver.driver.info.name,
+                        QDriver.driver.info.comment,
+                        QDriver.driver.info.phoneNumber,
+                        QDriver.driver.info.license,
                         car.seater,
                         car.number
                  ))
-                .from(driver)
-                .innerJoin(driver.cars, car)
+                .from(QDriver.driver)
+                .innerJoin(QDriver.driver.cars, car)
                 .where(
-                        driverIdEq(driverId),
+                        driverEq(driver),
                         carSeaterEq(cnt)
                 ).fetchOne());
     }
 
-    private BooleanExpression driverIdEq(Long driverId) {
-        return driver.id.eq(driverId);
+    private BooleanExpression driverEq(Driver driver) {
+        return QDriver.driver.eq(driver);
     }
 
     private BooleanExpression carSeaterEq(int cnt) {

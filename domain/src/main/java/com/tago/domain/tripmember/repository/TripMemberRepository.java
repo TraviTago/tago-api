723978824da50ef531869856e5e70bbc1861aa1e
twoosky <lhjae3@naver.com>
2023-10-11 17:44:06 +0900

    refactor: 사용 안하는 쿼리메서드 삭제

--- domain/src/main/java/com/tago/domain/tripmember/repository/TripMemberRepository.java
+++ domain/src/main/java/com/tago/domain/tripmember/repository/TripMemberRepository.java
@@ -1,5 +1,7 @@
 package com.tago.domain.tripmember.repository;
 
+import com.tago.domain.member.domain.Member;
+import com.tago.domain.trip.domain.Trip;
 import com.tago.domain.tripmember.domain.TripMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,7 +9,5 @@ import java.util.Optional;
 
 public interface TripMemberRepository extends JpaRepository<TripMember,Long>, TripMemberCustomRepository {
 
-    Boolean existsByTripIdAndMemberId(Long tripId, Long memberId);
-
-    TripMember findByTripIdAndMemberId(Long tripId, Long memberId);
+    Boolean existsByTripAndMember(Trip trip, Member member);
 }

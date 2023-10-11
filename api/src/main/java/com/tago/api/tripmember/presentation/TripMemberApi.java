c92febe47667708f692ff12e4d376cbaef0440cf
twoosky <lhjae3@naver.com>
2023-10-11 18:08:36 +0900

    refactor: tripmember API 통합

--- api/src/main/java/com/tago/api/tripmember/presentation/TripMemberApi.java
+++ api/src/main/java/com/tago/api/tripmember/presentation/TripMemberApi.java
@@ -4,6 +4,7 @@ package com.tago.api.tripmember.presentation;
 import com.tago.api.common.security.annotation.UserAuthentication;
 import com.tago.api.common.dto.ResponseDto;
 import com.tago.api.tripmember.application.TripMemberSaveService;
+import com.tago.api.tripmember.dto.response.TripMemberGetResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,17 +12,26 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
-public class TripMemberSaveApi {
+public class TripMemberApi {
 
     private final TripMemberSaveService tripMemberService;
 
-    @PostMapping("/trips/{tripId}/join")
-    public ResponseEntity<Void> joinTrip(
+    @PostMapping("/trips/{tripId}")
+    public ResponseEntity<Void> joinOrLeave(
             @PathVariable Long tripId,
+            @RequestParam("state") String state,
             @UserAuthentication Long memberId
     ){
-        tripMemberService.joinTrip(tripId, memberId);
+        tripMemberService.joinOrLeave(tripId, memberId, state);
         return ResponseDto.noContent();
+    }
+
+    @GetMapping("/trips/{tripId}/members")
+    public ResponseEntity<TripMemberGetResponse> getMembersByTrip(
+            @PathVariable Long tripId
+    ) {
+        TripMemberGetResponse response = tripMemberService.getMembersByTrip(tripId);
+        return ResponseDto.ok(response);
     }
 }
 

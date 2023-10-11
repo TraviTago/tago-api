5512fa912e55fe3ac7f2d23224c413fd6e9a4641
twoosky <lhjae3@naver.com>
2023-10-11 17:46:46 +0900

    feat: driver event producer 생성

---
+++ domain/src/main/java/com/tago/domain/driver/event/producer/DispatchEvent.java
@@ -0,0 +1,19 @@
+package com.tago.domain.driver.event.producer;
+
+import com.tago.domain.common.event.Event;
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DispatchEvent implements Event {
+    private String phoneNumber;
+    private String name;
+    private Action action;
+
+    @Getter
+    public enum Action {
+        CREATE, DELETE
+    }
+}

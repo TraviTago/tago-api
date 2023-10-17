a46cc6994062428a146b31ddd7b4fbab1e64af29
twoosky <lhjae3@naver.com>
2023-10-17 23:34:23 +0900

    fix: trip scheduler cron 형식 수정 (월/일 12시)

--- api/src/main/java/com/tago/api/trip/scheduler/TripScheduler.java
+++ api/src/main/java/com/tago/api/trip/scheduler/TripScheduler.java
@@ -1,15 +1,22 @@
 package com.tago.api.trip.scheduler;
 
+import com.tago.api.trip.scheduler.job.TripJob;
+import com.tago.api.trip.scheduler.job.TripJobFactory;
+import com.tago.api.trip.scheduler.job.TripJobType;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @EnableScheduling
 @Component
+@RequiredArgsConstructor
 public class TripScheduler {
 
-    @Scheduled(cron = "0 0 0 ? * MON *")
-    public void run() {
+    private final TripJobFactory tripJobFactory;
 
+    @Scheduled(cron = "0 0 12 ? * MON,SUN")
+    public void run() {
+        tripJobFactory.getInstance(TripJobType.CREATE_ORIGIN_TRIP).run();
     }
 }

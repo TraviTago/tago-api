61b4b5b12d13a1ab9431d05743c52e21201dbb89
twoosky <lhjae3@naver.com>
2023-10-17 12:39:38 +0900

    feat: phoneNumber에 해당하는 멤버 존재여부 반환 쿼리 메서드 구현

--- domain/src/main/java/com/tago/domain/member/repository/MemberRepository.java
+++ domain/src/main/java/com/tago/domain/member/repository/MemberRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {
     Optional<Member> findByPhoneNumber(String phoneNumber);
+    Boolean existsByPhoneNumber(String phoneNumber);
 }

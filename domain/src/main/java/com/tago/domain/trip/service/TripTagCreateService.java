9e42f3650422943fbb7aa81ac545424a4117826f
twoosky <lhjae3@naver.com>
2023-10-17 23:26:01 +0900

    feat: originTrip 생성 서비스 구현

--- domain/src/main/java/com/tago/domain/trip/service/TripTagCreateService.java
+++ domain/src/main/java/com/tago/domain/trip/service/TripTagCreateService.java
@@ -1,7 +1,6 @@
 package com.tago.domain.trip.service;
 
-import com.tago.domain.member.domain.vo.Favorite;
-import com.tago.domain.tag.domain.Tag;
+import com.tago.domain.tag.domain.vo.TagType;
 import com.tago.domain.tag.handler.TagQueryService;
 import com.tago.domain.trip.domain.Trip;
 import com.tago.domain.trip.domain.TripTag;
@@ -16,8 +15,8 @@ public class TripTagCreateService {
 
     private final TagQueryService tagQueryService;
 
-    public List<TripTag> createTripTags(Trip trip, List<Favorite> types) {
-        List<Tag> tags = tagQueryService.findByTypes(types);
+    public List<TripTag> createTripTags(Trip trip, List<TagType> types) {
+        List<com.tago.domain.tag.domain.Tag> tags = tagQueryService.findByTypes(types);
 
         return tags.stream()
                 .map(tag -> new TripTag(trip, tag))

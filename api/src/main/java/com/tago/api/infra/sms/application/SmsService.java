4051438c24649c72e907fd750a593f8ba29bd35f
twoosky <lhjae3@naver.com>
2023-10-17 12:41:03 +0900

    feat: verify-code api 응답으로  회원가입 여부 반환

--- api/src/main/java/com/tago/api/infra/sms/application/SmsService.java
+++ api/src/main/java/com/tago/api/infra/sms/application/SmsService.java
@@ -4,6 +4,7 @@ import com.tago.api.infra.sms.domain.SmsClient;
 import com.tago.api.infra.sms.domain.dto.SmsRequest;
 import com.tago.api.infra.sms.domain.dto.SmsResponse;
 import com.tago.api.infra.sms.util.VerificationCodeUtil;
+import com.tago.domain.member.handler.MemberQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ public class SmsService {
 
     private final SmsClient smsClient;
     private final VerificationCodeUtil verificationCodeUtil;
+    private final MemberQueryService memberQueryService;
 
     public void send(SmsRequest.Send request) {
         smsClient.send(request);
@@ -20,7 +22,13 @@ public class SmsService {
 
     public SmsResponse.VerifyCode verifyCode(SmsRequest.VerifyCode request) {
         verificationCodeUtil.verify(request.getNumber(), request.getCode());
-        return new SmsResponse.VerifyCode(true);
+        return new SmsResponse.VerifyCode(true, isSignUp(request.getNumber()));
+    }
+
+    public Boolean isSignUp(String number) {
+        String regEx = "(\\d{3})(\\d{3,4})(\\d{4})";
+        String phoneNumber = number.replaceAll(regEx, "$1-$2-$3");
+        return memberQueryService.existsByPhoneNumber(phoneNumber);
     }
 }
 

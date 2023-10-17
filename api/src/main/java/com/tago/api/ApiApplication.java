5e9f34d7698bae883ca4bc37c8f49120083d1a42
twoosky <lhjae3@naver.com>
2023-10-18 00:07:52 +0900

    feat: @EnableJpaAuditing 추가

--- api/src/main/java/com/tago/api/ApiApplication.java
+++ api/src/main/java/com/tago/api/ApiApplication.java
@@ -3,8 +3,10 @@ package com.tago.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @ConfigurationPropertiesScan
+@EnableJpaAuditing
 @SpringBootApplication(scanBasePackages = {"com.tago.api", "com.tago.domain"})
 public class ApiApplication {
 

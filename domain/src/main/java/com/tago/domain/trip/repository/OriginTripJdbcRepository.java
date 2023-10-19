9e00a283ff68d913b41d9cc813df8b22de4a28a0
twoosky <lhjae3@naver.com>
2023-10-20 03:19:19 +0900

    feat: tripPlace bulk Insert 구현

--- domain/src/main/java/com/tago/domain/trip/repository/OriginTripJdbcRepository.java
+++ domain/src/main/java/com/tago/domain/trip/repository/OriginTripJdbcRepository.java
@@ -1,10 +1,13 @@
 package com.tago.domain.trip.repository;
 
 import com.tago.domain.trip.dto.OriginTripCreateDto;
+import com.tago.domain.trip.dto.OriginTripPlaceCreateDto;
 
 import java.util.List;
 
 public interface OriginTripJdbcRepository {
 
-    void saveAll(List<OriginTripCreateDto> commands);
+    void saveAllTrip(List<OriginTripCreateDto> commands);
+    void saveAllTripPlace(List<OriginTripPlaceCreateDto> commands);
+    Long lastInsertId();
 }

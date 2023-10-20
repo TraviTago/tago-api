2014defc2a67a90b17b12359d4c1138918564466
twoosky <lhjae3@naver.com>
2023-10-20 17:24:16 +0900

    fix: fcm token redis key 수정

--- api/src/main/java/com/tago/api/infra/fcm/FCMTokenUtil.java
+++ api/src/main/java/com/tago/api/infra/fcm/FCMTokenUtil.java
@@ -9,21 +9,22 @@ import org.springframework.stereotype.Repository;
 public class FCMTokenUtil {
 
     private final RedisUtil redisUtil;
+    private static final String REDIS_KEY_TYPE = "fcm-token::";
     private static final long expiredTime = 60 * 24 * 60 * 60L;  // 2개월
 
     public void save(String key, String token) {
-        redisUtil.setDataExpire(key, token, expiredTime);
+        redisUtil.setDataExpire(REDIS_KEY_TYPE + key, token, expiredTime);
     }
 
     public String get(String key) {
-        return redisUtil.getData(key);
+        return redisUtil.getData(REDIS_KEY_TYPE + key);
     }
 
     public void delete(String key) {
-        redisUtil.delete(key);
+        redisUtil.delete(REDIS_KEY_TYPE + key);
     }
 
     public Boolean hasKey(String key) {
-        return redisUtil.hasKey(key);
+        return redisUtil.hasKey(REDIS_KEY_TYPE + key);
     }
 }

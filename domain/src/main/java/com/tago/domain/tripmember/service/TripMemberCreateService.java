07e54e59b3217760097a29fa11be880d4718ece2
twoosky <lhjae3@naver.com>
2023-11-20 05:10:36 +0900

    feat: 여행 참여하기/나가기 로직에 분산 락 적용

--- domain/src/main/java/com/tago/domain/tripmember/service/TripMemberCreateService.java
+++ domain/src/main/java/com/tago/domain/tripmember/service/TripMemberCreateService.java
@@ -7,7 +7,10 @@ import com.tago.domain.tripmember.event.producer.TripMemberEvent;
 import com.tago.domain.tripmember.event.producer.TripMemberEventProducer;
 import com.tago.domain.tripmember.exception.AlreadyExistsTripMemberException;
 import com.tago.domain.tripmember.service.factory.TripMemberService;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 
 
@@ -17,6 +20,7 @@ public class TripMemberCreateService implements TripMemberService {
 
     private static final String state = "ACCEPT";
     private final TripMemberEventProducer tripMemberEventProducer;
+    private final RedissonClient redissonClient;
 
     @Override
     public String getState() {
@@ -25,6 +29,25 @@ public class TripMemberCreateService implements TripMemberService {
 
     @Override
     public void action(Trip trip, Member member) {
+        String key = "LOCK-" + state + trip.getId();
+        RLock lock = redissonClient.getLock(key);
+
+        try {
+            boolean availableLock = lock.tryLock(3, 5, TimeUnit.SECONDS);
+
+            if (!availableLock) {
+                System.out.println("LOCK 획득 실패");
+                return;
+            }
+            create(trip, member);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void create(Trip trip, Member member) {
         validateJoinedAble(trip, member);
         trip.join(member);
         publishEvent(trip, member);
